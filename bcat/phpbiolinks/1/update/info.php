90caa6e8391ca5ae49b4e178fef0a6cb76d0284f
genshin-map <74144729+genshin-map@users.noreply.github.com>
2021-10-07 23:29:09 +0700

    2

---
+++ bcat/phpbiolinks/1/update/info.php
@@ -0,0 +1,15 @@
+<?php
+
+define('NEW_PRODUCT_VERSION', '11.1.0');
+define('NEW_PRODUCT_CODE', '1110');
+
+$updates = [
+    '801',
+    '900',
+    '910',
+    '920',
+    '930',
+    '1000',
+    '1100',
+    '1110',
+];
